1151e5837ea3f69b7788505eeff7648361d28d49
Alysha Gil <alyshanicole7@gmail.com>
2018-09-06 16:06:56 -0400

    add initial entries test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -81,7 +81,9 @@ $(function() {
             expect(body.classList.value).toBe('menu-hidden');
         });
     });
+    
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -90,10 +92,27 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('are present after feed is loaded', function(done) {
+            const feed = document.querySelector('.feed');
+            expect(feed.children.length).not.toBe(0);
+            done();
+        });
+    });
+
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+
+     });
 }());
